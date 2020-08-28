45e80cbd242ceaa0d4db3fada4b6e2886a54b7d3
gs2urq <gs2@juansemarquez.com>
2020-08-28 10:21:27 -0300

    Ingreso de precios y muestra de total

--- supermercado.py
+++ supermercado.py
@@ -1,9 +1,15 @@
 #! /usr/bin/python3
 
 # Inicializamos las variables
+suma = 0
+precio = int(input("Ingrese el precio del primer producto"))
 
 # Cargamos los productos
+while precio > 0:
+    suma = suma + precio
+    precio = int(input("Ingrese el precio del producto, 0 para finalizar: "))
 
 # Mostramos resultados finales
+print("El total es: $" , suma)
 
 
